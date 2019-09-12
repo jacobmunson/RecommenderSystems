91697961e429953ca6638617cb9c9ad7576bcd47
jacobmunson <43017899+jacobmunson@users.noreply.github.com>
2019-09-11 23:16:32 -0600

    Update lira_gaussian_clusters.R

--- lira_gaussian_clusters.R
+++ lira_gaussian_clusters.R
@@ -7,26 +7,41 @@
 source('~/GitHub/RecommenderSystems/recommender_systems_helper_functions.R')
 
 # Sample vectors
-x_1 = c(5,5,2,1,3,3,1)
+x_1 = c(3,5,2,1,3,3,1)
 x_2 = c(3,5,2,2,0,2,1)
 x_3 = c(0,1,3,2,3,1,4)
 x_4 = c(5,1,2,3,4,1,6)
-rbind(x_1,x_2,x_3,x_4)
+x_5 = c(1,2,3,4,5,5,2)
+x_6 = c(1,1,1,1,1,1,1)
+x_7 = c(2,2,1,3,4,1,6)
+x_8 = c(3,2,4,4,4,2,5)
+user_df = rbind(x_1,x_2,x_3,x_4,x_5,x_6,x_7,x_8)
 
 #var(c(x_1,x_2,x_3,x_4), na.rm = TRUE);var(c(c(x_1,x_2,x_3,x_4),-c(x_1,x_2,x_3,x_4)), na.rm = TRUE)
 
 # All pairwise differences - very, very inefficient implementation
+
 differences = c(x_1 - x_2, x_1 - x_3, x_2 - x_3, x_1 - x_4, x_2 - x_4, x_3 - x_4)
 (v1 = var(differences, na.rm = TRUE)) # "population" variance
 
 
+iter = 500000
+diff_df = matrix(data = 0, nrow = iter, ncol = ncol(user_df))
+for(i in 1:iter){
+  
+  x = user_df[sample(x = 1:nrow(user_df), size = 2, replace = FALSE),]
+  diff_df[i,1:ncol(x)] = x[1,] - x[2,]
+  
+}
+(mu_pop = mean(c(diff_df), na.rm = TRUE)); (v_pop = var(c(diff_df), na.rm = TRUE)); (sd_pop = sd(c(diff_df), na.rm = TRUE))
+
 # Plotting Distribution of all pairwise differences -
-plot(density(differences, na.rm = TRUE), ylim = c(0,1)); mean(differences, na.rm = TRUE)
+plot(density(rnorm(n = 100000, mean = mu_pop, sd = sd_pop)), ylim = c(0,1))
 
 
 # Selecting two vectors for a single pairwise comparison
-x_u = x_1 
-x_v = x_2 
+x_u = user_df[1,] 
+x_v = user_df[2,]
 (diff = x_u - x_v)
 var(diff, na.rm = TRUE)
 
@@ -36,11 +51,11 @@ lines(density(diff, na.rm = TRUE), col = "red")
 
 
 # Summary & Similarity Measures
-cat("\n", "Pop Var:", v1, "|", "Pairwise Var:", var(diff, na.rm = TRUE), "\n",
+cat("\n", "Pop Var:", v_pop, "|",
+    "Pairwise Var:", var(diff, na.rm = TRUE), "\n",
     "Cosine:", cosine_similarity(rbind(x_u,x_v))["x_u","x_v"], "\n",
     "Pearson:", cor(x_u, x_v, use = "pairwise.complete.obs"), "\n",
     "LiRa Unif:",lira(x_u, x_v, num_ratings = 5), "\n",
-    "LiRa Gauss:", (v1/var(diff, na.rm = TRUE)), "\n") #^length(diff)
-
+    "LiRa Gauss:", (v_pop/var(diff, na.rm = TRUE)), "\n") #^length(diff)
 
 
