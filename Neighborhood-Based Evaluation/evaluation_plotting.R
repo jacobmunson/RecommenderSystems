b7ab0ba0205e9f7c2d53bd0a685122ee299100f0
jacobmunson <43017899+jacobmunson@users.noreply.github.com>
2019-10-26 15:22:58 -0600

    Update evaluation_plotting.R

--- Neighborhood-Based Evaluation/evaluation_plotting.R
+++ Neighborhood-Based Evaluation/evaluation_plotting.R
@@ -6,20 +6,27 @@ head(D)
 unique(D$`Similarity Measure`)
 unique(D$k)
 
-D %>% filter(`Similarity Measure` == "Pearson Correlation - PWC" , Method == "kNN") %>% select(k, MAE)
+D %>% filter(`Similarity Measure` == "Pearson Correlation - PWC" , Method == "kNN") %>% group_by(k) %>% summarize(MAE = mean(MAE)) %>% select(k, MAE)
 D %>% filter(`Similarity Measure` == "Pearson Correlation - IZ", Method == "kNN") %>% select(k, MAE)
 D %>% filter(`Similarity Measure` == "Cosine Similarity", Method == "kNN") %>% select(k, MAE)
 D %>% filter(`Similarity Measure` == "LiRa - Uniform", Method == "kNN") %>% select(k, MAE)
 D %>% filter(`Similarity Measure` == "LiRa - Gaussian", Method == "kNN") %>% select(k, MAE)
 D %>% filter(`Similarity Measure` == "LiRa - LogGaussian", Method == "kNN") %>% select(k, MAE)
 
+D %>% filter(`Similarity Measure` == "Pearson Correlation - PWC" , Method == 'NN-RST') %>% select(k, MAE)
+D %>% filter(`Similarity Measure` == "Pearson Correlation - IZ", Method == 'NN-RST') %>% select(k, MAE)
+D %>% filter(`Similarity Measure` == "Cosine Similarity", Method == 'NN-RST') %>% select(k, MAE)
+D %>% filter(`Similarity Measure` == "LiRa - Uniform", Method == 'NN-RST') %>% select(k, MAE)
+D %>% filter(`Similarity Measure` == "LiRa - Gaussian", Method == 'NN-RST') %>% select(k, MAE)
+D %>% filter(`Similarity Measure` == "LiRa - LogGaussian", Method == 'NN-RST') %>% select(k, MAE)
+
 
 A = D %>% filter(Method == 'kNN') %>% ggplot(aes(x = k, y = MAE, group = `Similarity Measure`, color = `Similarity Measure`))  +
     geom_line(linetype="dashed", size=1.0) +
-    geom_point(color="black", size=1.2) + ggtitle("kNN Average")
+    geom_point(color="black", size=1.2) + ggtitle("kNN Average") + theme_grey() + ylim(0.74,0.78)
 B = D %>% filter(Method == 'NN-RST') %>% ggplot(aes(x = k, y = MAE, group = `Similarity Measure`, color = `Similarity Measure`))  +
     geom_line(linetype="dashed", size=1.0) +
-    geom_point(color="black", size=1.2) + ggtitle("Neighbor Approach")
+    geom_point(color="black", size=1.2) + ggtitle("Neighbor Approach") + theme_grey()
 
 library(cowplot)
 plot_grid(A, B)
