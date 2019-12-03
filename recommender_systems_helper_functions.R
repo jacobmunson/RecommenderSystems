31867548b8bbb7c817970ecdbcd0f9b96197c7c8
jacobmunson <43017899+jacobmunson@users.noreply.github.com>
2019-12-02 20:05:50 -0700

    Update recommender_systems_helper_functions.R

--- recommender_systems_helper_functions.R
+++ recommender_systems_helper_functions.R
@@ -89,6 +89,33 @@ lira_gaussian = function(x_u, x_v, sd_sc, sd_pc){
   return(lira)
 }
 
+lira_gaussian = function(x_u, x_v, sd_sc, lira_pure_chance_pdf){
+  
+  z_u = (x_u - mean(x_u, na.rm = T))/sd(x_u, na.rm = T); 
+  z_v = (x_v - mean(x_v, na.rm = T))/sd(x_v, na.rm = T)
+  
+  
+  diff = abs(x_u - x_v)
+  diff = diff[!is.na(diff)]
+  num_diff = length(diff)
+  diff_z = abs(z_u - z_v)
+  diff_z = diff_z[!is.na(diff_z)]
+  num_diff_z = length(diff_z)
+  
+  # same cluster
+  lira_top = prod(dnorm_diff(x = diff_z, mu = 0, sigma = sqrt(sum(c(sd_sc,sd_sc)^2))))
+  # pure chance
+  #lira_bottom = prod(dnorm_diff(x = diff_z, mu = 0, sigma = sqrt(sum(c(sd_pc,sd_pc)^2))))
+  
+  # pure chance
+  lira_bottom = prod(lira_pure_chance_pdf[names(table(diff)),]^table(diff))
+  
+  
+  lira = log10((lira_top/lira_bottom))
+  
+  return(lira)
+}
+
 lira_multinomial = function(x_u, x_v, multinomial_pure_chance_pdf, lira_same_cluster_pdf){
   
   diff = abs(x_u - x_v)
