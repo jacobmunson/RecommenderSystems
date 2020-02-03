e6d81c5b79d2eaf354238360a2320d0d3bed0688
jacobmunson <43017899+jacobmunson@users.noreply.github.com>
2020-02-03 13:54:38 -0700

    Update movielens_r_dotbase_input_script.R

--- Handling Large Data/movielens_r_dotbase_input_script.R
+++ Handling Large Data/movielens_r_dotbase_input_script.R
@@ -1,5 +1,23 @@
+
+### Mystery Machine ###
+
+
 D_train <- read.table("~/Documents/Recommender Systems - Mystery Machine/ml-100k/u1.base")
 colnames(D_train) = c("user","item","rating","timestamp")
 
 D_test <- read.table("~/Documents/Recommender Systems - Mystery Machine/ml-100k/u1.test")
+colnames(D_test) = c("user","item","rating","timestamp")
+
+
+
+
+
+
+
+### Corner Machine ###
+
+D_train <- read.table("~/Recommender Systems - Corner/ml-100k/u1.base")
+colnames(D_train) = c("user","item","rating","timestamp")
+
+D_test <- read.table("~/Recommender Systems - Corner/ml-100k/u1.test")
 colnames(D_test) = c("user","item","rating","timestamp")
