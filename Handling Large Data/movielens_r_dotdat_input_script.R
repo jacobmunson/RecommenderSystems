2df982ae50cbc3380fec5376cc63d60a1292d81e
Jacob-MM <jmnsn1@gmail.com>
2020-02-13 20:53:27 -0700

    Update movielens_r_dotdat_input_script.R

--- Handling Large Data/movielens_r_dotdat_input_script.R
+++ Handling Large Data/movielens_r_dotdat_input_script.R
@@ -1,5 +1,7 @@
 ratings <- read.table("Recommender Systems - Home Folder/ml-1m/ratings.dat")
 ratings <- read.table("Recommender Systems - Home Folder/ml-10M/ratings.dat")
+ratings <- read.table("~/Documents/Recommender Systems - Mystery Machine/ml-10M/ratings.dat")
+
 # Format user::item::rating::timestamp
 D = gsub(x = ratings$V1, pattern = "::", replacement = " ") # general substitution
 D = matrix(data = as.numeric(unlist(strsplit(D, "\\s+"))), ncol = 4, byrow = TRUE) # filling in matrix
