0c6760c1e263fede78a37a946a20c63ef87f8bc1
jacobmunson <43017899+jacobmunson@users.noreply.github.com>
2020-02-14 22:01:27 -0700

    Update movielens_r_dotbase_input_script.R

--- Handling Large Data/movielens_r_dotbase_input_script.R
+++ Handling Large Data/movielens_r_dotbase_input_script.R
@@ -33,13 +33,6 @@ D = read_csv("~/Recommender Systems - Home Folder/ml-latest-small-100k/ratings.c
 colnames(D) = c("user","item","rating","timestamp")
 
 ## 100k ML Benchmark
-D_train <- read.table("~/Recommender Systems - Home Folder/ml-100k/u1.base")
+D_train <- read.table("~/Recommender Systems - Home Folder/ml-100k/u5.base")
+D_test <- read.table("~/Recommender Systems - Home Folder/ml-100k/u5.test")
 
-######################
-### Corner Machine ###
-######################
-D_train <- read.table("~/Recommender Systems - Corner/ml-100k/u1.base")
-colnames(D_train) = c("user","item","rating","timestamp")
-
-D_test <- read.table("~/Recommender Systems - Home Folder/ml-100k/u1.test")
-colnames(D_test) = c("user","item","rating","timestamp")
