e1d61d1a81fc6977750350827b0e7181ca41a120
Jacob-MM <jmnsn1@gmail.com>
2020-03-18 22:57:34 -0600

    Update chunk_test_set.R

--- chunk_test_set.R
+++ chunk_test_set.R
@@ -1,6 +1,7 @@
 ## Splitting the dataset into smaller peices for parallel processing
+set.seed(1)
 chunk = function(vector, num_splits){return(split(vector, factor(sort(rank(vector) %% num_splits))))}
-dim(D_test)
-num_shards = detectCores()*50
+
+num_shards = detectCores()*70
 shards = chunk(vector = seq(1:nrow(D_test)), num_splits = num_shards)
 names(shards) = seq(1,num_shards,1)
