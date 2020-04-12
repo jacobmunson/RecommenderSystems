638a3ce5d8d1851d055ec6c0d630f550229ef800
Jacob-MM <jmnsn1@gmail.com>
2020-04-11 21:49:29 -0600

    Update movielens_r_dotdat_input_script.R

--- Handling Large Data/movielens_r_dotdat_input_script.R
+++ Handling Large Data/movielens_r_dotdat_input_script.R
@@ -7,3 +7,13 @@ D = gsub(x = ratings$V1, pattern = "::", replacement = " ") # general substituti
 D = matrix(data = as.numeric(unlist(strsplit(D, "\\s+"))), ncol = 4, byrow = TRUE) # filling in matrix
 head(D) # visual check
 dim(D) # how many? 
+
+
+# ratings <- read.table("Recommender Systems - Home Folder/ml-1m/ratings.dat")
+# ratings <- read.table("Recommender Systems - Home Folder/ml-10M/ratings.dat")
+# ratings <- read.table("Documents/Recommender Systems - Home Folder/ml-1m/ratings.dat")
+
+# Format user::item::rating::timestamp
+D = gsub(x = ratings$V1, pattern = "::", replacement = " ") # general substitution
+D = matrix(data = as.numeric(unlist(strsplit(D, "\\s+"))), ncol = 4, byrow = TRUE) # filling in matrix
+head(D) # visual check
