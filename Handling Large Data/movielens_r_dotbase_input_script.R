53949c7a2935be0e4ffd7d69d3ca42b5ad0e9b65
Jacob-MM <jmnsn1@gmail.com>
2020-04-11 21:51:54 -0600

    Update movielens_r_dotbase_input_script.R

--- Handling Large Data/movielens_r_dotbase_input_script.R
+++ Handling Large Data/movielens_r_dotbase_input_script.R
@@ -2,11 +2,26 @@
 #########################
 #### Mystery Machine ####
 #########################
+set = 5
+D_train <- read.table(file = paste0("~/Documents/Recommender Systems - Mystery Machine/ml-100k/u",set,".base"))
+colnames(D_train) = c("user","item","rating","timestamp")
+
+D_test <- read.table(file = paste0("~/Documents/Recommender Systems - Mystery Machine/ml-100k/u",set,".test"))
+colnames(D_test) = c("user","item","rating","timestamp")
+
+
+
 D_train <- read.table("~/Documents/Recommender Systems - Mystery Machine/ml-100k/u1.base")
 colnames(D_train) = c("user","item","rating","timestamp")
 
 D_test <- read.table("~/Documents/Recommender Systems - Mystery Machine/ml-100k/u1.test")
 colnames(D_test) = c("user","item","rating","timestamp")
+
+
+
+#path = paste0("~/Documents/Recommender Systems - Mystery Machine/ml-100k/u",set,".base")
+
+
 ########################
 
 
