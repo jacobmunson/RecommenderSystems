b9c21613b6b975033dd5e11f0f608e9495a2f1a3
bigblack <43017899+jacobmunson@users.noreply.github.com>
2020-08-24 04:54:27 -0600

    Update netflix_data_analysis_script.R

--- Handling Large Data/netflix_data_analysis_script.R
+++ Handling Large Data/netflix_data_analysis_script.R
@@ -1,9 +1,19 @@
+# Identifying the effects in the Koren (2009) paper
+
 library(lubridate)
 
 netflix_data = netflix_data %>% mutate(date = as_date(date))
+
+
+netflix_data %>% group_by(item) %>% tally() %>% summary()
+
+
 nf_by_date = netflix_data %>% group_by(date) %>% summarize(mu = mean(rating))
 
 mean(netflix_data$rating)
+
+
+
 
 library(ggplot2)
 nf_by_date %>% dim()
@@ -12,3 +22,4 @@ nf_by_date %>%
   ggplot(aes(x = date, y = mu)) + 
   geom_point(alpha = 0.35) + theme_bw() +
   ylab("Mean Rating") + xlab("Date") + ggtitle("Netflix Data", subtitle = "Mean rating through time")
+# looks about right
